0d9cf10c2ce8a7fb1935c35c5c790b84169e4845
Michal Bojanowski <michal2992@gmail.com>
2019-12-08 21:29:10 +0100

    Fix examples in data doc

--- R/EF3.R
+++ R/EF3.R
@@ -21,8 +21,8 @@
 #' @examples
 #' if( require(igraph, quietly = TRUE) ) {
 #'   set.seed(1)
-#'   plot(EFnet, layout=layout.fruchterman.reingold,
-#'     vertex.color=V(EFnet)$type+1, vertex.label.family="",
+#'   plot(EF3, layout=layout.fruchterman.reingold,
+#'     vertex.color=V(EF3)$type+1, vertex.label.family="",
 #'     sub="Source: Echenique & Fryer (2006)",
 #'     main="Neighborhood racial segregation\n in a fictional city"
 #'   )
