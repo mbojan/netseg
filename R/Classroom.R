a4e2b1e663e83889c3d64a5e01a8d0e801054cf0
Michal Bojanowski <michal2992@gmail.com>
2019-12-09 22:14:24 +0100

    Add ref to Dolata
    References #3.

--- R/Classroom.R
+++ R/Classroom.R
@@ -1,13 +1,17 @@
 #' Classroom network
 #'
 #' A directed network of ties "with whom do you like to play with?" collected in
-#' a school class of 9-year olds.
+#' a school class of 9-year olds. The data comes from a study undertook by
+#' Educational Research Institute (Dolata 2014).
 #'
 #' @format
 #' Object of class "igraph" with a directed network of size 26. Vertex
 #' attribute `gender`, takes values "Boy" or "Girl".
 #'
 #' @references
+#' Dolata, Roman (ed). (2014). *Czy szkoła ma znaczenie? Zróżnicowanie wyników
+#' nauczania po pierwszym etapie edukacyjnym oraz jego pozaszkolne i szkolne
+#' uwarunkowania*. Vol. 1.  Warsaw: Instytut Badań Edukacyjnych.
 #'
 #' @keywords datasets
 #' @docType data
