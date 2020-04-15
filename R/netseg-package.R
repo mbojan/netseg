489215f81223dad98b933c2ad6d45a7a1f59d835
Michal Bojanowski <michal2992@gmail.com>
2020-04-16 01:48:51 +0200

    Add reference to package man page

--- R/netseg-package.R
+++ R/netseg-package.R
@@ -1,4 +1,11 @@
 #' @keywords internal
+#' @docType package
+#'
+#' @references
+#' Bojanowski, Michał, and Rense Corten. 2014. "Measuring Segregation in Social
+#' Networks." *Social Networks* 39: 14–32.
+#' https://doi.org/10.1016/j.socnet.2014.04.001.
+#'
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
