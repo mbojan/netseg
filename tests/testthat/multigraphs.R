6075c75bb8099bffb1d36a2e1e591ada0c9282d1
Michal Bojanowski <michal2992@gmail.com>
2020-04-16 02:32:59 +0200

    Null models for multigraph mixingm

--- tests/testthat/multigraphs.R
+++ tests/testthat/multigraphs.R
@@ -1,8 +1,17 @@
 context("Checking multigraphs")
 
-
-g <- igraph::make_graph(~ A --+ B, A --+ B, C, simplify = FALSE)
+library(igraph)
+g <- igraph::make_graph(~ A --+ B, A --+ B, B --+ c, simplify = FALSE)
 V(g)$type <- c(1, 1, 2)
 plot(g)
 mixingm(g, "type")
-mixingm(g, "type", full=TRUE)
+a <- mixingm(g, "type", full=TRUE)
+d <- as.data.frame(as.table(a))
+
+# Null tie-based model
+fit1 <- glm(Freq ~ ego*tie + alter*tie + ego*alter, data=d, family=poisson("log"))
+predict(fit1, type="response")
+
+# Null contact model
+fit2 <- glm(Freq ~ ego + alter, data=d, family=poisson("log"), subset=tie == "TRUE")
+predict(fit2, type="response")
