0786445de57b7215b753b29ab25982222e377c4c
Michal Bojanowski <michal2992@gmail.com>
2021-02-11 11:31:28 +0100

    Update man of fold()

--- R/fold.R
+++ R/fold.R
@@ -3,21 +3,21 @@
 #' Fold a square matrix by collapsing lower triangle on upper triangle, or vice
 #' versa, through addition.
 #'
-#' By default, for \code{direction=="upper"}, the function takes the values in
-#' the lower triangle of \code{x} and adds them symetrically to the values in
+#' By default, for `direction=="upper"`, the function takes the values in
+#' the lower triangle of `x` and adds them symetrically to the values in
 #' the upper triangle. The values on the diagonal remain unchanged. The lower
-#' triangle is filled with 0s. If \code{direction=="lower"} the upper triangle
+#' triangle is filled with 0s. If `direction=="lower"` the upper triangle
 #' is collapsed on to the lower triangle.
 #'
 #' @param x square numeric matrix
 #'
-#' @param direction character, one of \code{"upper"} or \code{"lower"},
+#' @param direction character, one of `"upper"` or `"lower"`,
 #' direction of folding
 #'
-#' @return Square matrix of the same \code{dim} as \code{x} with the lower
+#' @return Square matrix of the same `dim` as `x` with the lower
 #' (upper) triangle folded onto the upper (lower) triangle.
 #'
-#' @seealso \code{\link{upper.tri}}, \code{\link{lower.tri}}
+#' @seealso [upper.tri()], [lower.tri()], [symmetrize()]
 #'
 #' @export
 #' @examples
